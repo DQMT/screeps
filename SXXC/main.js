e81cda93456b50faa89bc1430d9f542f99491a6c
dqmt <daoqiemantou@gmail.com>
2019-04-09 23:23:04 +0800

    update

--- SXXC/main.js
+++ SXXC/main.js
@@ -9,7 +9,7 @@ module.exports.loop = function () {
     }
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    // console.log('Upgrader: ' + upgraders.length +' Harvester: '+harvesters.length);
+    console.log('Upgrader: ' + upgraders.length +' Harvester: '+harvesters.length);
 
     //We can also use StructureSpawn.renewCreep to maintain the needed number of creeps.
     if(upgraders.length + harvesters.length < 15 && Game.spawns['shaxianxiaochi'].energy >200) {
--- SXXC/role.harvester.js
+++ SXXC/role.harvester.js
@@ -37,7 +37,9 @@ var roleHarvester = {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_EXTENSION ||
                     structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    structure.structureType == STRUCTURE_TOWER ||
+                    structure.structureType == STRUCTURE_CONTAINER ||
+                        structure.structureType == STRUCTURE_ROAD) && structure.energy < structure.energyCapacity;
             }
         });
         if (targets.length == 0 && creep.carry.energy == creep.carryCapacity) {
