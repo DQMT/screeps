f1c7e04fe48ffdc075d870b13b745ef4621a0da9
DQMT <daoqiemantou@gmail.com>
2019-04-09 19:16:32 +0800

    tincat

---
+++ tincat/main.js
@@ -0,0 +1,24 @@
+var supervisor = require('./command/supervisor');
+
+module.exports.loop = function () {
+
+    var tower = Game.getObjectById('d33a8ae27570153d7426a289');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        supervisor.urge(creep);
+    }
+}
