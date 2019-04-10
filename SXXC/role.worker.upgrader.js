0abab0ce860e8271fb0de6353050956a939f72c4
DQMT <daoqiemantou@gmail.com>
2019-04-10 23:13:22 +0800

    x

--- SXXC/role.worker.upgrader.js
+++ SXXC/role.worker.upgrader.js
@@ -17,17 +17,27 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.carry.energy == 0) {
-            var sources = getEnergySources(creep);
-            var source = util.getHashedTarget(creep,sources);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
-        }
-        else {
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
+			creep.memory.upgrading = false;
+			creep.say('harvest');
+		}
+		if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+			creep.memory.upgrading = true;
+			creep.say('upgrade');
+		}
+
+		if (creep.memory.upgrading) {
+			if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
+		}
+		else {
+			var sources = getEnergySources(creep);
+			var source = util.getHashedTarget(creep, sources);
+			if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
+			}
         }
     }
 };
