d30fb8eb1089eb55e7230e2be1e98475d34380b0
DQMT <daoqiemantou@gmail.com>
2019-04-10 22:32:50 +0800

    update

--- SXXC/role.worker.builder.js
+++ SXXC/role.worker.builder.js
@@ -5,34 +5,35 @@ var util = require('util');
  */
 var roleBuilder = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
+	/** @param {Creep} creep **/
+	run: function (creep) {
 
-	    if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('harvest');
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.say('build');
-	    }
+		if (creep.memory.building && creep.carry.energy == 0) {
+			creep.memory.building = false;
+			creep.say('harvest');
+		}
+		if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+			creep.memory.building = true;
+			creep.say('build');
+		}
 
-	    if(creep.memory.building) {
+		if (creep.memory.building) {
 			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-				var target = util.getHashedTarget(creep,targets);
-                if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-	    }
-	    else {
+			if (targets.length) {
+				var target = targets[0];
+				// var target = util.getHashedTarget(creep,targets);
+				if (creep.build(target) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+				}
+			}
+		}
+		else {
 			var sources = creep.room.find(FIND_SOURCES);
-			var source = util.getHashedTarget(creep,sources);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-	    }
+			var source = util.getHashedTarget(creep, sources);
+			if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
+			}
+		}
 	}
 };
 
