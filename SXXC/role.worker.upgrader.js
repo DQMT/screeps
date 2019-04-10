eafe844851afc0aeb3c097aa690ea061c2eb6912
DQMT <daoqiemantou@gmail.com>
2019-04-10 20:39:28 +0800

    update

--- SXXC/role.worker.upgrader.js
+++ SXXC/role.worker.upgrader.js
@@ -15,19 +15,19 @@ function getEnergySources(creep) {
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+    run: function (creep) {
+        if (creep.carry.energy == 0) {
             var sources = getEnergySources(creep);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
         else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
         }
-	}
+    }
 };
 
 module.exports = roleUpgrader;
