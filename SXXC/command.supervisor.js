a48fb4ed610b14ff92583e0550c1a82dd3b11d67
DQMT <daoqiemantou@gmail.com>
2019-04-11 23:18:59 +0800

    x

--- SXXC/command.supervisor.js
+++ SXXC/command.supervisor.js
@@ -34,7 +34,7 @@ var theSupervisor = {
         if (Memory.peace && (!structureSpawn.spawning) && Memory.limits['harvesters'] < Memory.limits['upgraders'] && totalSpawnEnergy < totalSpawnEnergyCapacity) {
             util.setLimit('harvesters', 4);
         }
-        if (structureSpawn.room.find(FIND_CONSTRUCTION_SITES).length && Memory.peace && (!structureSpawn.spawning) && builders < Memory.limits['upgraders']) {
+        if (structureSpawn.room.find(FIND_CONSTRUCTION_SITES).length && Memory.peace && (!structureSpawn.spawning) && Memory.limits['builders'] < Memory.limits['upgraders']) {
             util.setLimit('builders', 4);
         }
     },
