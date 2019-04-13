2aad8981842aff145760a967e9fe0ec8c883b979
DQMT <daoqiemantou@gmail.com>
2019-04-13 15:42:58 +0800

    update

--- SXXC/role.worker.harvester.js
+++ SXXC/role.worker.harvester.js
@@ -33,8 +33,8 @@ function getEnergyContainers(creep) {
                 structure.structureType == STRUCTURE_SPAWN ||
                 structure.structureType == STRUCTURE_EXTENSION ||
                 structure.structureType == STRUCTURE_TOWER
-            ) && structure.energy < structure.energyCapacity) || ((structure.structureType == STRUCTURE_CONTAINER ||
-                structure.structureType == STRUCTURE_STORAGE) && structure.store['energy'] < structure.storeCapacity);
+            ) && structure.energy < structure.energyCapacity);
+            //  || ((structure.structureType == STRUCTURE_CONTAINER ||  structure.structureType == STRUCTURE_STORAGE) && structure.store['energy'] < structure.storeCapacity);
         }
     });
     return targets;
