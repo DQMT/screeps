b34b728b0503dfb46943f9577d6ebc406bb4d5b9
DQMT <daoqiemantou@gmail.com>
2019-04-13 15:33:44 +0800

    update

--- SXXC/role.worker.harvester.js
+++ SXXC/role.worker.harvester.js
@@ -54,7 +54,7 @@ var roleHarvester = {
         }
         if (creep.memory.transfering) {//transfer
             var structure;
-            if (!creep.memory.structure || (structure = Game.getObjectById(creep.memory.structure)) == null || structure.energy == structure.energyCapacity) {
+            if (creep.memory.structure==null || (structure = Game.getObjectById(creep.memory.structure)) == null || structure.energy == structure.energyCapacity) {
                 var targets = getEnergyContainers(creep);
                 if (targets.length > 0) {
                     creep.memory.structure = targets[0]['id'];
