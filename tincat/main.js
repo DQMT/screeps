ab5ea8dde3dba2aec377270b2e92985b4cae2f9d
DQMT <daoqiemantou@gmail.com>
2019-04-15 19:33:44 +0800

    tincat2.0

--- tincat/main.js
+++ tincat/main.js
@@ -4,16 +4,16 @@ var watchdog = require('./command.watchdog');
 
 module.exports.loop = function () {
     // system.clear();
-    system.init(['sim']);
-    system.cleanMemory();
-    system.setLimits({
-        harvesters: 5,
-        upgraders: 2,
-        builders: 2,
-        repairers: 2,
-        mrhandys:1
-    });
-    watchdog.watchEverything();
-    supervisor.keep();
-    supervisor.urge();
+    // system.init(['W5S37'],['W5S38','W4S37']);
+    // system.cleanMemory();
+    // system.setLimits({
+    //     harvesters: 5,
+    //     upgraders: 2,
+    //     builders: 2,
+    //     repairers: 2,
+    //     mrhandys:1
+    // });
+    // watchdog.watchEverything();
+    // supervisor.keep();
+    // supervisor.urge();
 }
