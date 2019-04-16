30c174ec4afdd2b861fa4eb87f201d4e23e43de8
DQMT <daoqiemantou@gmail.com>
2019-04-16 12:23:10 +0800

    tincat2.0

--- tincat/main.js
+++ tincat/main.js
@@ -3,20 +3,17 @@ var supervisor = require('./command.supervisor');
 var watchdog = require('./command.watchdog');
 
 module.exports.loop = function () {
-    // system.clear();
-    system.init(['W5S37'],['W5S38','W4S37']);
+    system.clear();
+    system.init(['W17N44']);
     system.cleanMemory();
     system.setLimits({
-        harvesters: 20,
-        upgraders: 6,
-        builders: 3,
-        repairers: 2,
-        mrhandys:1
+        harvesters: 5,
+        upgraders: 3,
+        builders: 1,
+        repairers: 0,
+        mrhandys:0
     });
-    system.setMaxBinds('5bbcac9e9099fc012e635dce',5);
-    system.setMaxBinds('5bbcac9e9099fc012e635dcf',5);
-    system.setMaxBinds('5bbcacae9099fc012e63600f',7);
-    system.setMaxBinds('5bbcacae9099fc012e636010',7);
+    // system.setMaxBinds('5bbcac9e9099fc012e635dce',5);
     watchdog.watchEverything();
     supervisor.keep();
     supervisor.keepDefence();
