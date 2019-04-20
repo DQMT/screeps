58c647c18c2b10a3d694044ccc40ba0f0b79137c
DQMT <daoqiemantou@gmail.com>
2019-04-20 10:00:34 +0800

    x

--- tincat/main.js
+++ tincat/main.js
@@ -9,14 +9,14 @@ module.exports.loop = function () {
     system.init(['E47N38']);
     system.cleanMemory();
     system.setLimits({
-        harvesters: 3,
+        harvesters: 6,
         upgraders: 2,
         builders: 1,
         repairers: 0,
         mrhandys:0
     });
     system.setMaxBinds('5bbcafcb9099fc012e63b33d',3);
-    system.setMaxBinds('5bbcafcb9099fc012e63b33e',2);
+    system.setMaxBinds('5bbcafcb9099fc012e63b33e',3);
     // system.setMaxDrills('5bbcabeb9099fc012e634810',1,0,1,0);
     // system.setMaxDrills('5bbcabeb9099fc012e634811',1,0,1,0);
     watchdog.watchEverything();
