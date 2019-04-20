6af950a61f648b080b4700901f158317c5532581
DQMT <daoqiemantou@gmail.com>
2019-04-20 19:01:07 +0800

    x

--- tincat/job.js
+++ tincat/job.js
@@ -3,10 +3,15 @@ var constants = require('./constants');
 
 module.exports = {
     exeTempJpb: function () {
-        var structureSpawn = Game.spawns[constants.MY_SPAWN_NAMES[0]];
-        structureSpawn.spawnCreep(roleScout.body(),
-            roleScout.newName(),
-            { memory: { role: 'repairer' } }
-        );
+        // var structureSpawn = Game.spawns[constants.MY_SPAWN_NAMES[0]];
+        // structureSpawn.spawnCreep(roleScout.body(),
+        //     roleScout.newName(),
+        //     { memory: { role: 'scout' } }
+        // );
+        
+        var scouts = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+        scouts.forEach(element => {
+            element.memory.role='scout';
+        });
     }
 };
