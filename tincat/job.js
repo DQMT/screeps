1f391e19d15b6046b92572c34b81290a7bce6eec
DQMT <daoqiemantou@gmail.com>
2019-04-20 19:16:23 +0800

    x

--- tincat/job.js
+++ tincat/job.js
@@ -1,5 +1,6 @@
 var roleScout = require('./role.scout');
 var constants = require('./constants');
+var util = require('./util');
 
 module.exports = {
     exeTempJpb: function () {
@@ -8,15 +9,24 @@ module.exports = {
         //     roleScout.newName(),
         //     { memory: { role: 'scout' } }
         // );
-        
+
         var scouts = _.filter(Game.creeps, (creep) => creep.memory.role == 'scout');
         scouts.forEach(element => {
-            element.memory.roomName='E48N38';
-            element.moveTo(47,18);
+            element.memory.roomName = 'E48N38';
+            element.moveTo(47, 18);
         });
-        var creep = Game.creeps['Repairer@1_6119790'];
-        creep.memory.role='repairer';
-        creep = Game.creeps['Repairer@1_6119913'];
-        creep.memory.role='repairer';
+
+        var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+        repairers.forEach(e => {
+            e.memory.busy = true;
+            if (e.room.name != 'E48N38') {
+                util.moveToRoom(e, 'E48N38');
+            }else{
+                var target = Game.getObjectById('5cb9eb8264eeab642c5fb25b');
+                e.dismantle(target);
+            }
+        });
+        
+
     }
 };
