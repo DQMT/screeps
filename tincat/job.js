bc1a2f675eeedd41eff3a2b80ff9853d63bb4671
DQMT <daoqiemantou@gmail.com>
2019-04-20 09:49:14 +0800

    x

--- tincat/job.js
+++ tincat/job.js
@@ -1,8 +1,9 @@
 
 module.exports = {
     job:function(){
-        var creep = Game.creeps['Driller@1_6109124'];
+        var creep = Game.creeps['Harvester@1_6110520'];
         console.log(' i am builder now ');
-        creep.memory.role= 'builder';
+        creep.memory.source= '5bbcafcb9099fc012e63b33d';
+        creep.memory.bindSource= '5bbcafcb9099fc012e63b33d';
     }
 };
