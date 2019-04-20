3a9e89260dc727e711f346c5bcc84d94ea3c561d
DQMT <daoqiemantou@gmail.com>
2019-04-20 10:30:12 +0800

    x

--- tincat/job.js
+++ tincat/job.js
@@ -1,11 +1,10 @@
 
 module.exports = {
     exeTempJpb: function () {
-        if (creep.name == 'Upgrader@1_6110940') {
-            if (creep.signController(creep.room.controller, "Leave me alone, please.") == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-                return;
-            }
+        var creep = Game.creeps['Upgrader@1_6110940'];
+        if (creep.signController(creep.room.controller, "Leave me alone, please.") == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
+            return;
         }
     }
 };
