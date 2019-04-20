7720ab2561ee2c7eba37b280b25db94f0a77ade0
DQMT <daoqiemantou@gmail.com>
2019-04-21 06:39:34 +0800

    x

--- tincat/job.js
+++ tincat/job.js
@@ -1,32 +1,12 @@
 var roleScout = require('./role.scout');
 var constants = require('./constants');
 var util = require('./util');
+var system = require('./system');
 
 module.exports = {
     exeTempJpb: function () {
-        // var structureSpawn = Game.spawns[constants.MY_SPAWN_NAMES[0]];
-        // structureSpawn.spawnCreep(roleScout.body(),
-        //     roleScout.newName(),
-        //     { memory: { role: 'scout' } }
-        // );
-
-        // var scouts = _.filter(Game.creeps, (creep) => creep.memory.role == 'scout');
-        // scouts.forEach(element => {
-        //     element.memory.roomName = 'E48N38';
-        //     element.moveTo(47, 18);
-        // });
-
-        var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
-        repairers.forEach(e => {
-            e.memory.busy = true;
-            if (e.room.name != 'E48N38') {
-                util.moveToRoom(e, 'E48N38');
-            }else{
-                var target = Game.getObjectById('5cb9eb8264eeab642c5fb25b');
-                e.dismantle(target);
-            }
-        });
-
+        
+        system.addColony('E48N38',['5bbcafdc9099fc012e63b4ca','5bbcafdc9099fc012e63b4c8']);
 
     }
 };
