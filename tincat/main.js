70347e8ec88cc6aa4eb09827a315974c4d5d9320
DQMT <daoqiemantou@gmail.com>
2019-05-11 08:52:40 +0800

    x

--- tincat/main.js
+++ tincat/main.js
@@ -15,13 +15,13 @@ module.exports.loop = function () {
         upgraders: 4,
         builders: 6,
         repairers: 0,
-        mrhandys:1,
-        miners:1
+        mrhandys: 1,
+        miners: 0
     });
-    system.setMaxBinds('5bbcafcb9099fc012e63b33d',4);
-    system.setMaxBinds('5bbcafcb9099fc012e63b33e',4);
-    system.setMaxBinds('5bbcafdc9099fc012e63b4ca',4);
-    system.setMaxBinds('5bbcafdc9099fc012e63b4c8',5);
+    system.setMaxBinds('5bbcafcb9099fc012e63b33d', 4);
+    system.setMaxBinds('5bbcafcb9099fc012e63b33e', 4);
+    system.setMaxBinds('5bbcafdc9099fc012e63b4ca', 4);
+    system.setMaxBinds('5bbcafdc9099fc012e63b4c8', 5);
     // system.setMaxDrills('5bbcabeb9099fc012e634810',1,0,1,0);
     // system.setMaxDrills('5bbcabeb9099fc012e634811',1,0,1,0);
     watchdog.watchEverything();
