a173ad11ff901827ece88d06b8a6ae95c79142a4
DQMT <daoqiemantou@gmail.com>
2020-06-14 16:38:22 +0800

    constants

---
+++ constants.js
@@ -0,0 +1,20 @@
+var WORKER_STATE = {
+    HARVEST: 'harvest',
+    UPGRADE: 'upgrade',
+    BUILD: 'build',
+}
+
+var STROKE_COLOR = {
+    HARVEST:'#FFFFFF',
+    TRANSFER:'#CDCD00',
+    BUILD:'#1874CD',
+    REPAIR:'#1874CD',
+    UPGRADE:'#4B0082',
+    ATTACK:'#CD0000',
+    LORRY:'#000000'
+}
+
+module.exports = {
+    WORKER_STATE:WORKER_STATE,
+    STROKE_COLOR:STROKE_COLOR
+};
